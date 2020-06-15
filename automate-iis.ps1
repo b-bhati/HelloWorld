b0d4835988d0e7edecaa9328b6f7b0678af56228
Bhoopendra Singh <bhoopendra.bits@outlook.com>
2020-06-15 22:27:44 +0530

    Update automate-iis.ps1

--- automate-iis.ps1
+++ automate-iis.ps1
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-Add-WindowsFeature Web-Server
-Set-Content -Path "C:\inetpub\wwwroot\iisstart.html" -Value "Hello World from updated host $($env:computername) !"
+Set-Content -Path "C:\inetpub\wwwroot\iisstart.htm" -Value "Hello World from updated host $($env:computername) !"
